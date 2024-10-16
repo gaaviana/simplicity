7a10ee4933a7b4578888b5c2e7c1b68476d4ea2d
Gabriel Viana <gaa.vianaa@gmail.com>
2024-10-16 11:26:03 -0300

    fim

--- js/contato.js
+++ js/contato.js
@@ -71,3 +71,36 @@ botaoBuscar.addEventListener("click", async function () {
     
 
 }); //final do evento do botao
+
+
+// codigo formspree
+var form = document.getElementById("my-form");
+  
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Obrigado! Mensagens enviada com sucesso. Agurade nosso retorno.";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! Algo de errado não esta certo... Tente novamente mais tarde"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Algo de errado não esta certo... Tente novamente mais tarde"
+  });
+}
+form.addEventListener("submit", handleSubmit)
